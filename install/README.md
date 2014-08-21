8f24f17f2fc0bfea36808fd1069e9cdf2f529cf3
ethanpeng <ethanpeng76@gmail.com>
2014-08-21 11:17:02 +0800

    Add install script

---
+++ install/README.md
@@ -0,0 +1,8 @@
+OpenACS Environment
+===================
+
+## Install
+
+execute install.sh and leave the MySQL root password blank.
+
+- ubuntu 14.04 tested
---
+++ install/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+PACKAGE_DIR=./package
+
+echo "install JDK ..."
+if ! command -v java > /dev/null; then
+    sudo apt-get install -y default-jdk
+fi
+
+echo "install ant ..."
+if ! command -v ant > /dev/null; then
+    sudo apt-get install -y ant
+fi
+
+echo "install MYSQL ..."
+if ! command -v mysql > /dev/null; then
+    sudo apt-get install -y mysql-server
+    echo "create database openacs" | mysql -uroot
+fi
+
+echo "install JBoss ..."
+if [ ! -f "/opt/jboss/bin/run.jar" ]; then
+    wget http://sourceforge.net/projects/jboss/files/JBoss/JBoss-4.2.3.GA/jboss-4.2.3.GA-jdk6.zip
+    sudo mkdir -p /opt/jboss
+    unzip jboss-4.2.3.GA-jdk6.zip > /dev/null
+    sudo mv jboss-4.2.3.GA/* /opt/jboss
+    rm -rf jboss-4.2.3.GA
+fi
+
+echo "install mysql connector java ..."
+if [ ! -f "/opt/jboss/server/default/lib/mysql-connector-java.jar" ]; then
+    sudo apt-get install -y libmysql-java
+    sudo ln -sf /usr/share/java/mysql-connector-java.jar /opt/jboss/server/default/lib/
+fi
+
+echo "install openacs configure file ..."
+if [ ! -f "/opt/jboss/server/default/deploy/openacs-ds.xml" ]; then
+    cp $PACKAGE_DIR/users.properties $PACKAGE_DIR/roles.properties /opt/jboss/server/default/conf
+    cp $PACKAGE_DIR/openacs-ds.xml /opt/jboss/server/default/deploy
+fi
+
+echo "================================================================================"
+echo "check java version ..."
+java -version
+
+echo "check ant version ..."
+ant -version
+
+echo "================================================================================"
+echo "web url: http://localhost:8080/openacs"
+echo "user: admin"
+echo "password: openacs"
+
